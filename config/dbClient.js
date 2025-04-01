56b6cb7af9001004aa0ade0fdcb726655ccb993c
alexiscaos <151911625+alexiscaos@users.noreply.github.com>
2025-04-01 08:51:47 +0200

    prueba de giitignore

--- config/dbClient.js
+++ config/dbClient.js
@@ -5,8 +5,8 @@ class dbClient {
     constructor(){
         //Recordar de cambiar el user y psswd por una variable 
         //que se denominara en el archivo .env que se encuentra en 
-        //la carpeta principal
-        const queryString = 'mongodb+srv://smartFlora:KX7XAf9L6ahDgJAx@cluster0.xvhmbng.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+        //la carpeta principal, el enlace de aqui ya no sirve por lo mismo
+        const queryString = 'mongodb+srv://smartFlora:KX7XAf9L6ahDgJAx@cluster0.xvhmbng.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; 
         this.client = new MongoClient(queryString);
         this.conectarBD();
     }
