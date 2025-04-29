b145f8d7a378d2d40b15b4b6c4c3618113c24b24
Luisa Stoian <lustoiann@gmail.com>
2025-04-29 14:57:19 -0600

    base_Front

---
+++ FrontEnd/smart_flora_front_end/src/components/Navigator.jsx
@@ -0,0 +1,16 @@
+function Navigator(){
+    return(
+        <div className="navContainer">
+            <img src="" alt="" className="smartFlora_Logo" />
+            <nav className="navigator">
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </nav>
+        </div>
+    )
+}
+
+export default Navigator
