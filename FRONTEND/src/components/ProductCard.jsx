7acc5a53e76b2c6400d508cc63dd7831584f73f2
Luisa Stoian <lustoiann@gmail.com>
2025-05-21 10:44:30 +0200

    creada la plantilla de Producto Final, modificación las llamadas a las imagenes de cada producto de la bbdd

--- FRONTEND/src/components/ProductCard.jsx
+++ FRONTEND/src/components/ProductCard.jsx
@@ -2,9 +2,14 @@ import { Link } from 'react-router-dom';
 
 
 const ProductCard = ({ product }) => {
-  const imageUrl = product.imageUrl.startsWith("/uploads")
-    ? `http://localhost:3000${product.imageUrl}`
-    : product.imageUrl;
+
+  
+   const cardImage = product.images?.find(img => img.id === "Card");
+
+   const imageUrl = cardImage?.url?.startsWith("/uploads")
+      ? `http://localhost:3000${cardImage.url}`
+      : cardImage?.url || "default-pot.jpg";
+
 
     
   return (
@@ -14,7 +19,7 @@ const ProductCard = ({ product }) => {
         alt={product.name || product.nombre}
         className="w-full rounded-lg mb-4"
       />
-      <p>{product._id}</p>
+      {/* <p>{product._id}</p> */}
       <h3 className="text-2xl font-bold mb-2">{product.name || product.nombre}</h3>
       <p className="text-sm mb-2">{product.description || product.descuento || "Sin descripción"}</p>
       <p className="text-lg font-semibold mb-4">${product.price || product.precio}</p>      
